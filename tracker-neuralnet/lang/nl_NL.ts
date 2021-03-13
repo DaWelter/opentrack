e2a087321f3ed5aca4e9984ef83fcd9db1ce97f8
Michael Welter <michael@welter-4d.de>
2021-03-13 11:27:03 +0100

    Add initial working prototype for a face alignment based tracker
    Powered by AI!

---
+++ tracker-neuralnet/lang/nl_NL.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl_NL">
+<context>
+    <name>Form</name>
+    <message>
+        <source>Tracker settings</source>
+        <translation>Tracker-instellingen</translation>
+    </message>
+    <message>
+        <source>Frames per second</source>
+        <translation>Frames per seconde</translation>
+    </message>
+    <message>
+        <source>Camera name</source>
+        <translation>Cameranaam</translation>
+    </message>
+    <message>
+        <source>Diagonal FOV</source>
+        <translation>Diagonale FOV</translation>
+    </message>
+    <message>
+        <source>Camera settings</source>
+        <translation>Camera-instellingen</translation>
+    </message>
+    <message>
+        <source>Offset Forward</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Offset Up</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>neuralnet_dialog</name>
+    <message>
+        <source>Default</source>
+        <translation type="unfinished">Standaard</translation>
+    </message>
+</context>
+</TS>
---
+++ tracker-neuralnet/lang/ru_RU.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>Form</name>
+    <message>
+        <source>Tracker settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Diagonal FOV</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Camera settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Frames per second</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Camera name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Offset Forward</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Offset Up</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>neuralnet_dialog</name>
+    <message>
+        <source>Default</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
